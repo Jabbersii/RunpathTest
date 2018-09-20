16ce9472d24e59c68ef39b234d4c9e933250c796
Lewis Stapleton <lewis.stapleton@outlook.com>
2018-09-20 20:48:47 +0100

    Refactor loading datatable into script

---
+++ src/Web/wwwroot/js/index.js
@@ -0,0 +1,22 @@
+﻿var initDataTable = function (requestUrl) {
+    $('#photoTable').DataTable({
+        ajax: {
+            url: requestUrl,
+            dataSrc: ''
+        },
+        columns: [
+            { data: 'photoTitle' },
+            { data: 'albumName' },
+            {
+                data: 'thumbnailUrl',
+                render: function (data, type, row) {
+                    return '<a href="' + row.url + '">' +
+                        '<img src="' + data + '"/>' +
+                        '</a>';
+                },
+                orderable: false,
+                searchable: false
+            }
+        ]
+    });
+}
