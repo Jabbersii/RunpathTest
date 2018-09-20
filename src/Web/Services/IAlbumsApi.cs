1a344bd0409974880f39beacbcf59c8be68face3
Lewis Stapleton <lewis.stapleton@outlook.com>
2018-09-20 18:08:11 +0100

    Add REST API interfaces

---
+++ src/Web/Services/IAlbumsApi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestEase;
+using Web.Services.Models;
+
+namespace Web.Services
+{
+    public interface IAlbumsApi
+    {
+        [Get("albums")]
+        Task<List<Album>> GetAlbums([Query("id")]IEnumerable<int> ids);
+
+        [Get("albums/{id}")]
+        Task<Album> GetAlbum(int id);
+    }
+}
---
+++ src/Web/Services/IPhotosApi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestEase;
+using Web.Services.Models;
+
+namespace Web.Services
+{
+    public interface IPhotosApi
+    {
+        [Get("photos")]
+        Task<List<Photo>> GetPhotos();
+
+        [Get("photos/{id}")]
+        Task<Photo> GetPhoto(int id);
+    }
+}
