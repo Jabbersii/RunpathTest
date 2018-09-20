1fe33a1cbab7dec1ef39154b93948ca937e12ded
Lewis Stapleton <lewis.stapleton@outlook.com>
2018-09-20 13:40:42 +0100

    Implement outline of index page
    Implement a basic index page using jquery datatables
    with ajax using dummy data

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Web.Models;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
@@ -9,6 +11,29 @@ namespace Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult Data()
+        {
+            var photos = new[]
+            {
+                new PhotoViewModel()
+                {
+                    PhotoTitle = "accusamus beatae ad facilis cum similique qui sunt",
+                    AlbumName = "quidem molestiae enim",
+                    Url = new Uri("https://via.placeholder.com/600/92c952"),
+                    ThumbnailUrl = new Uri("https://via.placeholder.com/150/92c952")
+                },
+                new PhotoViewModel()
+                {
+                    PhotoTitle = "accusamus beatae ad facilis cum similique qui sunt",
+                    AlbumName = "quidem molestiae enim",
+                    Url = new Uri("https://via.placeholder.com/600/92c952"),
+                    ThumbnailUrl = new Uri("https://via.placeholder.com/150/92c952")
+                }
+            };
+
+            return Json(photos);
         }
 
         public IActionResult About()
---
+++ src/Web/ViewModels/PhotoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Web.ViewModels
+{
+    public class PhotoViewModel
+    {
+        public string PhotoTitle { get; set; }
+        public string AlbumName { get; set; }
+        public Uri ThumbnailUrl { get; set; }
+        public Uri Url { get; set; }
+    }
+}
